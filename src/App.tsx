3b2d42d84da71f5b04bcbe597d0ff29f243fdff2
aliozben <aliozben4@gmail.com>
2023-12-13 18:32:41 +0300

    Revert "feat: Added navbar component."
    This reverts commit 9f46388839e5834914d81c451cacafeb0e33e0c5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import {Container} from "react-bootstrap";
-import {Route, Routes} from "react-router-dom";
-import {Store} from "./pages/Store";
-import {Checkout} from "./pages/Checkout";
-import {Item} from "./pages/Item";
-import {Navbar} from "./components/Navbar";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import { Store } from "./pages/Store";
+import { Checkout } from "./pages/Checkout";
+import { Item } from "./pages/Item";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Store />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/item" element={<Item />} />
-        </Routes>
-      </Container>
-    </>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Store />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/item" element={<Item />} />
+      </Routes>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-min.css";
 import {BrowserRouter} from "react-router-dom";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
