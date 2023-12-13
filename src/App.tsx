e4121511fc97f2ef4aff97f91ca17350e2dec7c9
aliozben <aliozben4@gmail.com>
2023-12-13 18:25:58 +0300

    feat: Created and rotued new pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import { Store } from "./pages/Store";
+import { Checkout } from "./pages/Checkout";
+import { Item } from "./pages/Item";
+
 function App() {
-  return <></>;
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Store />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/item" element={<Item />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
